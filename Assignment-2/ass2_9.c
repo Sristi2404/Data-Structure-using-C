4030e84b8f5095f8245e2e76ac0094330a7198b8
Sristi_MCA_A_2024-26 <97033814+Sristi2404@users.noreply.github.com>
2024-10-15 00:54:00 +0530

    Update ass2_9.c

--- Assignment-2/ass2_9.c
+++ Assignment-2/ass2_9.c
@@ -1,3 +1,6 @@
+/*Write a program to allocate memory using calloc( ) and then reallocate the previously
+allocated memory using realloc( ). Display the elements which have been taken after
+reallocation.*/
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
