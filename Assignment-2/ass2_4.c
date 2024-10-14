2b5aaa1d3f9677c10ef6eb121e8432e9b32da79d
Sristi_MCA_A_2024-26 <97033814+Sristi2404@users.noreply.github.com>
2024-10-15 00:51:53 +0530

    Update ass2_4.c

--- Assignment-2/ass2_4.c
+++ Assignment-2/ass2_4.c
@@ -1,3 +1,5 @@
+/*Write a program that reads two 2D metrices from the console, verifies if metrics
+multiplication is possible or not. Then multiplies the metrices and prints the 3rd metrics.*/
 #include<stdio.h>
 #define MAX 10
 
